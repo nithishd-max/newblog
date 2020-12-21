a3be253b8324d8eeac687dcaaabb0ac52438fc13
nihisd-max <nithishdnr3@gmail.com>
2020-12-21 19:30:16 +0530

    v6

--- src/Interests.js
+++ src/Interests.js
@@ -5,7 +5,7 @@ const Interests =() =>(
     <div className="resume-section-content">
         <h2 className="mb-5">Interests</h2>
         <p>Apart from being a web developer, My day starts with Meditation and Yoga, I enjoy most of my time being outdoors. I am an avid chess and badminton player. I enjoy cycling and cooking.</p>
-        <p className="mb-0">When forced indoors, I follow a number of sci-fi and fantasyand comedy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+        <p className="mb-0">When forced indoors, I follow a number of sci-fi and fantasy and comedy genre movies and television shows, I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
     </div>
 </section>
 );
